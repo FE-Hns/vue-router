3f495f3f2c9b0840a9b9a7b960828307c2c043ba
hns <hunaisong@163.com>
2017-09-19 16:52:19 +0800

    router-link中的属性

--- src/App.vue
+++ src/App.vue
@@ -37,8 +37,8 @@
     <button @click="go">go</button> -->
     <!-- <transition name="fade" mode="out-in"> -->
       <ul>
-        <li><router-link to="/">/</router-link></li>
-        <li><router-link to="/parent">/parent</router-link></li>
+        <li><router-link to="/" >/</router-link></li>
+        <li><router-link to="/parent" >/parent</router-link></li>
         <li><router-link to="/parent/foo">/parent/foo</router-link></li>
         <li><router-link to="/parent/bar">/parent/bar</router-link></li>
       </ul>
